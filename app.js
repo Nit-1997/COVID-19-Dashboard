b16a92ba377dc5334a10c57e835fda74b5925a47
nitin <nitinb@cisco.com>
2020-04-16 21:34:46 +0530

    basic-setup

---
+++ app.js
@@ -0,0 +1,32 @@
+var  express        = require("express")
+   , app            = express()
+   , cors           = require('cors')
+   , mongoose       = require("mongoose")
+   , bodyParser     = require("body-parser");
+
+const path = require("path");
+mongoose.connect('mongodb://nitin:nitin1979@ds159574.mlab.com:59574/prepzone');
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+
+const dashboard = require('./routes/dashboard');
+
+
+app.use('/', dashboard);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+// const port  = process.env.PORT||7000;
+// app.listen(port,process.env.IP,function(){
+//      console.log("app server has started on heroku ");
+// });
+
+app.listen(7000,function(){
+     console.log("app server has started on 7000");
+});
+
---
+++ models/dashboard.js
@@ -0,0 +1,6 @@
+var mongoose    = require("mongoose");
+var DashboardSchema = new mongoose.Schema({
+
+});
+
+module.exports = mongoose.model("Dashboard",DashboardSchema);
